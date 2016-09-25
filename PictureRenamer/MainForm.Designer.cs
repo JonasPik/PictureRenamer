6c2f3c2194f701a6bca37d3a5ab55e358bb7ad3f
Jonas Pikiotas <j.pikiotas@gmail.com>
2016-09-25 20:52:06 +0100

    Initial Commit

---
+++ PictureRenamer/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿using System.ComponentModel;
+
+namespace PictureRenamer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ChooseFolderButton = new System.Windows.Forms.Button();
+            this.FolderPathLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ResultBox = new System.Windows.Forms.RichTextBox();
+            this.myWorker = new System.ComponentModel.BackgroundWorker();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(550, 11);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(78, 41);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ChooseFolderButton
+            // 
+            this.ChooseFolderButton.Location = new System.Drawing.Point(459, 12);
+            this.ChooseFolderButton.Name = "ChooseFolderButton";
+            this.ChooseFolderButton.Size = new System.Drawing.Size(85, 40);
+            this.ChooseFolderButton.TabIndex = 1;
+            this.ChooseFolderButton.Text = "Choose...";
+            this.ChooseFolderButton.UseVisualStyleBackColor = true;
+            this.ChooseFolderButton.Click += new System.EventHandler(this.ChooseFolderButton_Click);
+            // 
+            // FolderPathLabel
+            // 
+            this.FolderPathLabel.AutoSize = true;
+            this.FolderPathLabel.Location = new System.Drawing.Point(3, 39);
+            this.FolderPathLabel.Name = "FolderPathLabel";
+            this.FolderPathLabel.Size = new System.Drawing.Size(0, 13);
+            this.FolderPathLabel.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CancelBtn);
+            this.panel1.Controls.Add(this.ChooseFolderButton);
+            this.panel1.Controls.Add(this.FolderPathLabel);
+            this.panel1.Controls.Add(this.StartButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(731, 56);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.statusStrip1);
+            this.panel2.Controls.Add(this.ResultBox);
+            this.panel2.Location = new System.Drawing.Point(12, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(731, 276);
+            this.panel2.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 254);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(731, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Location = new System.Drawing.Point(6, 3);
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(722, 243);
+            this.ResultBox.TabIndex = 0;
+            this.ResultBox.Text = "";
+            // 
+            // myWorker
+            // 
+            this.myWorker.WorkerReportsProgress = true;
+            this.myWorker.WorkerSupportsCancellation = true;
+            this.myWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.myWorker_DoWork);
+            this.myWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.myWorker_ProgressChanged);
+            this.myWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.myWorker_RunWorkerCompleted);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(634, 12);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(79, 40);
+            this.CancelBtn.TabIndex = 3;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 368);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "PictureRenamer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ChooseFolderButton;
+        private System.Windows.Forms.Label FolderPathLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox ResultBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private BackgroundWorker myWorker;
+        private System.Windows.Forms.Button CancelBtn;
+    }
+}
+
---
+++ PictureRenamer/MainForm.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PictureRenamer
+{
+    public partial class MainForm : Form
+    {
+        public string FolderPath;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+
+            StartButton.Enabled = false;
+
+            CancelBtn.Enabled = false;
+        }
+
+        /// <summary>
+        /// Start Buttton Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (!myWorker.IsBusy)
+            {
+                StartButton.Enabled = false;
+
+                ChooseFolderButton.Enabled = false;
+
+                ResultBox.Text = string.Empty;
+
+                CancelBtn.Enabled = true;
+
+                myWorker.RunWorkerAsync(FolderPath);
+            }
+        }
+
+        /// <summary>
+        /// Choose Folder Button Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChooseFolderButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog
+            {
+                SelectedPath = !string.IsNullOrEmpty(FolderPath) ? FolderPath : @"C:\"
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                FolderPathLabel.Text = dialog.SelectedPath;
+
+                FolderPath = dialog.SelectedPath;
+
+                StartButton.Enabled = true;
+
+            }
+        }
+
+        /// <summary>
+        /// Cancel Button Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelBtn_Click(object sender, EventArgs e)
+        {
+            myWorker.CancelAsync();
+        }
+
+        /// <summary>
+        /// Get Date Taken property from image
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private string GetDateTakenFromImage(string path)
+        {
+            try
+            {
+                var myImage = Image.FromFile(path);
+
+                var propItem = myImage.GetPropertyItem(36867);
+
+                var datetime = new Regex(":").Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2).Replace(':', '_');
+
+                myImage.Dispose();
+
+                return datetime.Substring(0, datetime.Length - 1);
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Do Work
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void myWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var sendingWorker = (BackgroundWorker)sender;
+
+            var files = new DirectoryInfo(e.Argument.ToString()).GetFiles();
+
+            ProcessList(files, sendingWorker, e);
+
+            files = null;
+
+            e.Result = "Complete";
+        }
+
+        /// <summary>
+        /// Worker Work Completed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void myWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled && e.Error == null)
+            {
+                StatusLabel.Text = "Done";
+
+                ResultBox.Text += e.Result.ToString();
+            }
+            else if (e.Cancelled)
+            {
+                StatusLabel.Text = "Cancelled";
+            }
+            else
+            {
+                StatusLabel.Text = "An error has occurred";
+            }
+
+            // Re-enable the start button
+            StartButton.Enabled = true;
+
+            CancelBtn.Enabled = false;
+
+            ChooseFolderButton.Enabled = true;
+        }
+
+        /// <summary>
+        /// Worker Progress Changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void myWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //Show the progress to the user based on the input we got from the background worker
+            StatusLabel.Text = $"Photo Processed: {e.ProgressPercentage}...";
+
+            ResultBox.Text = (e.UserState.ToString());
+        }
+
+        /// <summary>
+        /// Proccess file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="sb"></param>
+        public void ProcessFile(FileInfo file, StringBuilder sb)
+        {
+            var newName = GetDateTakenFromImage(file.FullName);
+
+            var ext = "." + file.Name.Split('.')[1];
+
+            // If "Date Taken" property is not found, then don't rename
+            if (!string.IsNullOrEmpty(newName))
+            {
+                var destination = file.DirectoryName + "\\" + newName;
+
+                while (File.Exists(destination + ext))
+                {
+                    destination += "_0";
+                }
+
+                File.Move(file.FullName, destination + ext);
+            }
+            else
+            {
+                sb.AppendLine($"{file.Name} Was NOT renamed.");
+            }
+
+            sb.AppendLine($"{file.Name} was processed.");
+        }
+
+        /// <summary>
+        /// Process file list
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="worker"></param>
+        /// <param name="e"></param>
+        public void ProcessList(FileInfo[] files, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            var count = 0;
+
+            var sb = new StringBuilder();
+
+            foreach (var file in files)
+            {
+                if (!worker.CancellationPending)
+                {
+                    if (file.Name.EndsWith(".jpg", StringComparison.InvariantCultureIgnoreCase)
+                        || file.Name.EndsWith(".jpeg", StringComparison.InvariantCultureIgnoreCase)
+                        || file.Name.EndsWith(".png", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        ProcessFile(file, sb);
+
+                        count++;
+
+                        //Report our progress to the main thread
+                        worker.ReportProgress(count, sb);
+                    }
+
+                }
+                else
+                {
+                    //If a cancellation request is pending, assign this flag a value of true
+                    e.Cancel = true; 
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
